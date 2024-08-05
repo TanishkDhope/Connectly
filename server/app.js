a735c35edfd6327e9652db5e01a36100e8657fa5
Tanishk Dhope <tanishkdhope@gmail.com>
2024-08-05 22:13:05 +0530

    Version 1 Chat App

---
+++ server/app.js
@@ -0,0 +1,47 @@
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
+
+const User={};
+const app = express();
+const PORT = 3000;
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+io.on("connection", (socket) => {
+  console.log("User Connected ", socket.id);
+  //Store Name in Array
+  socket.on("new-user", (name) => {
+    User[socket.id] = name;
+    socket.broadcast.emit("user-connect", User[socket.id]);
+  });
+
+  socket.on("chat-message", (message) => {
+    socket.broadcast.emit("message", {
+      name: User[socket.id],
+      message: message,
+    });
+    socket.emit("personal-message", `You: ${message}`)
+  });
+
+  //On Disconnect
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnect", User[socket.id]);
+    console.log("User Disconnectd");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
